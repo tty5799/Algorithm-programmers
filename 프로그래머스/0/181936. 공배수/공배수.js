a3ac5b1d519f22bacd6444ff31029ba8efa03c19
C-D <101077946+tty5799@users.noreply.github.com>
2024-12-23 22:09:19 +0900

    [level 0] Title: 공배수, Time: 0.04 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181936. 공배수/공배수.js
@@ -0,0 +1,10 @@
+function solution(number, n, m) {
+    var answer = 0;
+    
+    if(number % n === 0 && number % m === 0) {
+        return answer = 1
+    } else {
+        return answer = 0
+    }
+    return answer;
+}
