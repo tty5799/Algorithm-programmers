dd915a0ef5ed16cbb09ec4be08e266de6e5e0f8c
C-D <101077946+tty5799@users.noreply.github.com>
2025-01-24 00:16:47 +0900

    [Silver I] Title: 단지번호붙이기, Time: 92 ms, Memory: 9396 KB -BaekjoonHub

---
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.js
@@ -0,0 +1,43 @@
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const input = require('fs').readFileSync(filePath).toString().trim().split('\n');
+
+const N = Number(input.shift())
+const map = input.map((v) => v.split('').map(Number));
+const visited = Array.from({length: N},() => Array(N).fill(false));
+
+let result = []
+let count = 0
+let home = 0
+
+const dy = [0,1,0,-1]
+const dx = [1,0,-1,0]
+
+function dfs(y,x) {
+    visited[y][x] = true
+    count++
+
+    for(let i=0; i < 4; i++) {
+        const ny = y + dy[i]
+        const nx = x + dx[i]
+
+        if(ny >= 0 && ny < N && nx >= 0 && nx < N && !visited[ny][nx] && map[ny][nx]){
+            dfs(ny,nx)
+        }
+    }
+}
+
+for (let i = 0; i < N; i++) {
+    for (let j = 0; j < N; j++) {
+        if (map[i][j] && !visited[i][j]) {
+            dfs(i, j);
+            home++
+            result.push(count)
+            count = 0
+        }
+    }
+}
+
+
+console.log(
+    home + "\n" + `${result.sort((a, b) => a - b).join("\n")}`
+);
