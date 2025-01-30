f19c8a221a9f9e0d30168e71f040b1c353bdbee6
C-D <101077946+tty5799@users.noreply.github.com>
2025-01-30 23:04:25 +0900

    [Silver II] Title: 섬의 개수, Time: 172 ms, Memory: 12196 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.js
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.js
@@ -1,12 +1,11 @@
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 const input = require('fs').readFileSync(filePath).toString().trim().split('\n');
 
-let W, H, graph, visited
+let w, h, graph, visited
+
 
 function dfs(y,x) {
-    if(visited[y][x]) return;
-
-    visited[y][x] = true;
+    visited[y][x] = 1
 
     const dy = [0,1,1,1,0,-1,-1,-1]
     const dx = [1,1,0,-1,-1,-1,0,1]
@@ -15,41 +14,36 @@ function dfs(y,x) {
         const ny = y + dy[i]
         const nx = x + dx[i]
 
-        if(ny >= 0 && ny < H && nx >= 0 && nx < W && graph[ny][nx]){
-            dfs(ny,nx);
+        if(ny >= 0 && ny < h && nx >= 0 && nx < w && graph[ny][nx] && !visited[ny][nx]) {
+            dfs(ny,nx)
         }
     }
-
 }
 
 function find() {
     let cnt = 0
 
-    for(let i = 0; i < H; ++i) {
-        for(let j = 0; j < W; ++j) {
-            if(graph[i][j] && !visited[i][j]){
+    for(let i = 0; i < h; ++i) {
+        for(let j = 0; j < w; ++j) {
+            if (graph[i][j] && !visited[i][j]) {
                 dfs(i,j)
                 cnt++
             }
         }
     }
-
     console.log(cnt)
 }
 
-
-
 while (input.length) {
-    [W, H] = input.shift().split(' ').map(Number);
+    [w, h] = input.shift().split(' ').map(Number);
+    graph = []
+    visited = Array.from(Array(h), () => Array(w).fill(0))
 
-    if (W === 0 && H === 0) break;
+    if(!w && !h) break;
 
-    visited = Array.from({ length: H }, () => Array(W).fill(false));
-    graph = [];
-
-    for (let j = 0; j < H; ++j) {
+    for(let i = 0; i < h; ++i) {
         graph.push(input.shift().split(' ').map(Number));
     }
 
-    find();
+    find()
 }
