7451a0a165667c1b0f0d02993ee15135121d6810
C-D <101077946+tty5799@users.noreply.github.com>
2025-01-30 21:49:47 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 944 ms, Memory: 131876 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.js
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.js
@@ -1,32 +1,36 @@
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 const input = require('fs').readFileSync(filePath).toString().trim().split('\n');
 
-const [N,M] = input.shift().split(' ').map(Number);
-const arr = input.map((v) => v.split(' ').map(Number));
-const graph = Array.from(Array(N + 1),() => []);
-const visited = Array(N).fill(0)
+const [N, M] = input.shift().split(' ').map(Number);
 
-for(const [form,to] of arr){
-    graph[form].push(to)
-    graph[to].push(form)
-}
+const arr = input.map((v) => v.split(' ').map(Number));
+const graph = Array.from(Array(N + 1), () => [])
+const visited = Array(N + 1).fill(0)
+
 
 let answer = 0
 
+for(const [from, to] of arr){
+    graph[from].push(to)
+    graph[to].push(from)
+}
+
+
 function dfs(node) {
-    if(visited[node - 1]) return;
-    visited[node - 1] = 1;
+    if(!visited[node]) {
+        visited[node] = 1
 
-    for(const next of graph[node]){
-        dfs(next)
+        for(const next of graph[node]){
+            dfs(next)
+        }
     }
 }
 
-
-for(let i = 0; i < N; ++i) {
-    if(!visited[i]) {
-        dfs(i + 1)
+for(let i = 0; i < N; ++i){
+    if(!visited[i + 1]) {
         answer++
+        dfs(i + 1)
     }
 }
+
 console.log(answer)
